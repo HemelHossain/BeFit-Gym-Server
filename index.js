505c0e6142e44c13b56d1054e1f38ad65c41399d
HemelHossain <hemelhossain533@gmail.com>
2023-05-31 07:44:03 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const service = require('./Data/Service.json');
+const product = require('./Data/Product.json');
+const price = require('./Data/Price.json');
+const blog = require('./Data/Blog.json');
+
+app.get('/', (req, res) => {
+    res.send('Dragon news server is running');
+});
+
+app.get('/service', (req, res) => {
+    res.send(service);
+});
+app.get('/product', (req, res) => {
+    res.send(product);
+});
+app.get('/price', (req, res) => {
+    res.send(price);
+});
+app.get('/blog', (req, res) => {
+    res.send(blog);
+});
+
+app.listen(port, () => {
+    console.log(`dragon news running on port, ${port}`);
+})
